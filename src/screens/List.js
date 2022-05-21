7a4f933fe8e685da81f556b6724bbb32c69fc6f8
*D8uZD3ENJO <shirakura@colors.jp>
2022-05-21 22:20:25 +0900

    code cleanup

--- src/screens/List.js
+++ src/screens/List.js
@@ -17,54 +17,30 @@ import { useNavigation, useRoute } from '@react-navigation/native';
 export default function List() {
   const navigation = useNavigation();
   const [data, setData] = useState([])
-  const [isLoading, setIsLoading] = useState(false)
   const [word, setWord] = useState('')
   const [viewData, setViewData] = useState([])
-  const [limit, setLimit] = useState(10)
+  const [page, setPage] = useState(0)
 
   useEffect(() => {
     fetchData()
-  }, [])
+  }, [page])
 
   useEffect(() => {
-    if(!word) {
-      setViewData(data)
-    } else {
-      const filtered = data.filter((v) => v.name.toLowerCase().includes(word))
-      setViewData(filtered)
-    }
-  }, [word])
+    const filtered = data.filter((v) => v.name.toLowerCase().includes(word))
+    setViewData(filtered)
+  }, [word, data])
 
   const fetchData = async() => {
     try {
-      setIsLoading(true)
-      const res = await axios.get(`https://example-data.draftbit.com/restaurants?_limit=${limit}`)
-      setData(res.data)
-      setViewData(res.data)
+      const res = await axios.get(`https://example-data.draftbit.com/restaurants?_page=${page}`)
+      setData(prev => prev.concat(res.data))
     } catch(e) {
       console.log('error', e)
-    } finally {
-      setIsLoading(false)
     }
   }
 
-  const loadMore = async() => {
-    try {
-      setIsLoading(true)
-      setLimit(prev => prev + 10)
-      const res = await axios.get(`https://example-data.draftbit.com/restaurants?_limit=${limit}`)
-      setData(res.data)
-      if(!word) {
-        setViewData(res.data)
-      } else {
-        const filtered = data.filter((v) => v.name.toLowerCase().includes(word))
-        setViewData(filtered)
-      }
-    } catch(e) {
-      console.log('error', e)
-    } finally {
-      setIsLoading(false)
-    }
+  const loadMore = () => {
+    setPage(prev => prev + 1)
   }
 
   const renderHeader = () => {
@@ -82,8 +58,8 @@ export default function List() {
 
   return (
     <View style={styles.container}>
-      {!isLoading?
-      <>
+      {data.length?
+        <>
         <View style={styles.searchContainer}>
           <SearchBox
             value={word}
@@ -91,8 +67,9 @@ export default function List() {
           />
         </View>
         <FlatList
-          data={viewData}
+          data={word?viewData:data}
           ListHeaderComponent={renderHeader}
+          keyExtractor={(item, index) => index.toString()}
           renderItem={({ item }) => {
             return (
               <RenderItem item={item} />
